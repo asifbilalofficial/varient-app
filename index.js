9a0f5ff7e27c1b8f4c563b539101cabea5bb555d
asifbilalofficial <130508631+asifbilalofficial@users.noreply.github.com>
2026-02-21 19:43:46 +0500

    Refactor create-variant endpoint to use request body
    Updated the create-variant endpoint to accept parameters from the request body instead of hardcoded values.

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ app.get("/", (req, res) => res.send("Server is alive"));
 
 // POST endpoint to create a single variant for multiple sizes
 app.post("/create-variant", async (req, res) => {
-  // let { product_id, option_name, price, weight } = req.body;
-  let product_id=10211240378642;
-  let option_name=UniqueOption;
-  let price=1;
-  let weight=2;
+   let { product_id, option_name, price, weight } = req.body;
   
 
   if (!product_id || !option_name || !price || !weight) {
@@ -108,4 +104,5 @@ app.post("/create-variant", async (req, res) => {
 // Start server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 
